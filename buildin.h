f507c2e3c2ef4406e590ed216f5834ed06aefe76
Tomáš Paulus <tpaulus@localhost.localdomain>
2014-11-21 16:47:18 +0100

    [buildin] Fixed comments, function prototypes etc.

--- buildin.h
+++ buildin.h
@@ -9,35 +9,47 @@
 #define __BUILDIN_H__
 
 #include "ial.h"
+#include "cstring.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 /**
-* @brief Counting characters in string and returns number of characters.
-*
-* @param  str char* String whose characters we want to count.
-* @return     int   Number of characters in string.
-*/
-int length(char *str);
+ * @brief Counting characters in cstring and returns length of cstring.
+ *
+ * @param  s cstring String whose characters we want to count.
+ * @return   int       Number of characters in string.
+ */
+int length(cstring *s);
 
 /**
- * @brief Copy characters from the character in the string at position i and copy
+ * @brief Copy characters from cstring in to the cstring from position i and copy
  * as much characters as the value of n is.
  *
- * @param  s char*  String from which is copied substring.
- * @param  i int    Index at which a substring starts.
- * @param  n int    Number of characters which are copied.
- * @return   char*  Returns copied substring.
+ * @param  s cstring    String from which is copied substring.
+ * @param  i int        Index at which a substring starts.
+ * @param  n int        Number of characters which are copied.
+ * @return   cstring    Returns copied substring.
  */
-char *copy(char *s, int i, int n);
+cstring *copy(const cstring *s, int i, int n);
 
 /**
- * @brief Sorting string characters by ordinal value.
+ * @brief Sorting cstring characters by ordinal value.
  *
- * @param  s char* String which will be sorted.
- * @return   char* Sorted string.
+ * @param  s cstring String which will be sorted.
+ * @return   cstring String which was sorted.
  */
-char *sort(char *s);
+cstring *sort(cstring *s);
+
+/**
+ * @brief Finds first occurence of substring and returns index where substring starts.
+ *        If searched substring is empty, returns position 1.
+ *
+ * @param  s        cstring    String which is sweeped for substring.
+ * @param  search   cstring    Substring which is searched for.
+ * @return          int        Returns index where ẃas first occurence of substring found.
+ *                             If searched substring is empty, returns position 1.
+ */
+int find(cstring *s, cstring *search);
 
 #endif /* #define __BUILDIN_H__ */
