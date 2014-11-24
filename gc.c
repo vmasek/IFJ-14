f8f09ea4a1a18a327cfef208a62e4a8ad25f2ded
Pavel Tobias <anotherpavel@gmail.com>
2014-11-24 15:40:05 +0100

    [gc] simple garbage collector implemented - see usage example in gc.h

---
+++ gc.c
@@ -0,0 +1,119 @@
+/**
+ * @file    gc.c
+ * @name    Garbage Collector
+ * @author  Pavel Tobias (xtobia01)
+ * @brief   Implementation of simple garbage collector
+ ****************************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "gc.h"
+
+struct branch {
+    char *tag;
+    unsigned length;
+    void **data;
+    struct branch *next;
+};
+
+static struct branch *create_branch(const char *tag);
+static void free_branch(struct branch **branch_ptr, const char *tag);
+static void *realloc_branch(struct branch *branch, size_t size);
+
+void gc_free(const char *tag)
+{
+    gc_malloc(tag, 0);
+}
+
+void *gc_malloc(const char *tag, size_t size)
+{
+    static struct branch *first;
+    struct branch **branch_ptr = &first;
+
+    if (tag == NULL)
+        return NULL;
+
+    if (!size) {
+        free_branch(&first, tag);
+        return NULL;
+    }
+
+    while (*branch_ptr != NULL) {
+        if (!strcmp((*branch_ptr)->tag, tag))
+            break;
+        branch_ptr = &(*branch_ptr)->next;
+    }
+
+    if (*branch_ptr == NULL) {
+        if ((*branch_ptr = create_branch(tag)) == NULL)
+            return NULL;
+    }
+
+    return realloc_branch(*branch_ptr, size);
+}
+
+static struct branch *create_branch(const char *tag)
+{
+    struct branch *branch;
+
+    if ((branch = malloc(sizeof(struct branch))) == NULL)
+        return NULL;
+
+    if ((branch->tag = malloc(strlen(tag) + 1)) == NULL) {
+        free(branch);
+        return NULL;
+    }
+
+    strcpy(branch->tag, tag);
+    branch->length = 0;
+    branch->data = NULL;
+    branch->next = NULL;
+
+    return branch;
+}
+
+static void free_branch(struct branch **branch_ptr, const char *tag)
+{
+    struct branch *branch;
+
+    while (*branch_ptr != NULL) {
+        if (!strcmp((*branch_ptr)->tag, tag))
+            break;
+        branch_ptr = &(*branch_ptr)->next;
+    }
+
+    if (*branch_ptr == NULL)
+        return;
+
+    branch = *branch_ptr;
+    *branch_ptr = branch->next;
+
+    while (branch->length--)
+        free(branch->data[branch->length]);
+
+    free(branch->data);
+    free(branch->tag);
+    free(branch);
+}
+
+static void *realloc_branch(struct branch *branch, size_t size)
+{
+    void *pointer;
+    void **temp_data;
+
+    if ((pointer = malloc(size)) == NULL)
+        return NULL;
+
+    if ((temp_data = realloc(branch->data, (branch->length + 1) * sizeof(void *)))
+        == NULL) {
+        free(pointer);
+        return NULL;
+    }
+
+    branch->data = temp_data;
+    branch->data[branch->length] = pointer;
+    branch->length++;
+
+    return pointer;
+}
---
+++ gc.h
@@ -0,0 +1,44 @@
+/**
+ * @file    gc.h
+ * @name    Garbage Collector (interface)
+ * @author  Pavel Tobias (xtobia01)
+ * @brief   Interface for simple garbage collector
+ ****************************************************************************/
+
+#ifndef __GC_H__
+#define __GC_H__
+
+#include <stdlib.h>
+
+void *gc_malloc(const char *tag, size_t size);
+void gc_free(const char *tag);
+
+/*  USAGE EXAMPLE:
+
+    #include "gc.h"
+
+    int main()
+    {
+        int *pepa_int = gc_malloc("pepik", sizeof(int));
+        float *pepa_float = gc_malloc("pepik", sizeof(float));
+
+        int *karel_int = gc_malloc("karlik", sizeof(int));
+        float *karel_float = gc_malloc("karlik", sizeof(float));
+
+        if (pepa_int == NULL || pepa_float == NULL ||
+            karel_int == NULL || karel_float == NULL)
+            goto cleanup;   // SOME OF THEM COULD'VE BEEN ALLOCATED SUCCESSFULLY
+
+        gc_free("karlik");  // KAREL GAVE UP ALL MEMORY HE HAS ALLOCATED
+        *pepa_int = 42;     // PEPA CAN STILL USE HIS MEMORY
+
+    cleanup:
+        gc_free("pepik");
+        gc_free("karlik");  // ( TRYING TO DEALLOCATE NEVER-ALLOCATED OR ALREADY
+        gc_free("jarda");   //   DEALLOCATED MEMORY DOESN'T DO HARM )
+
+        return 0;
+    }
+*/
+
+#endif
