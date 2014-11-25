59cc00527f3f3de75ecbc6c9ded95599b55eaa77
Pavel Tobias <anotherpavel@gmail.com>
2014-11-26 00:59:37 +0100

    [stack] stack_insert() fixed

--- stack.c
+++ stack.c
@@ -142,42 +142,26 @@ int stack_read_first_of_type(Stack *stack, int type, void **value)
 
 int stack_insert(Stack *stack, int searched_type, int type, void *value)
 {
-	if(!stack)
-	{
-		debug("No stack given");
-		return INTERNAL_ERROR;
-	}
-	if(!stack->top)
-	{
-		debug("Stack is empty");
-		return INTERNAL_ERROR;
-	}
+    Stack_Node **found_ptr = &stack->top;
+    Stack_Node *new_node;
 
-	Stack_Node *found = stack_find_first_type(stack, searched_type);
-	if(found)
-	{
+    if (stack == NULL || *found_ptr == NULL)
+        return INTERNAL_ERROR;
 
-		Stack_Node *tmp = malloc(sizeof(Stack_Node)); ///try using calloc and time diff
+    for (unsigned i = 0; i < stack->count; i++) {
+        if ((*found_ptr)->type != searched_type) {
+            found_ptr = &(*found_ptr)->next;
+            continue;
+        }
+        if ((new_node = malloc(sizeof(Stack_Node))) == NULL)
+            return INTERNAL_ERROR;
+        *new_node = (Stack_Node) {type, value, *found_ptr};
+        *found_ptr = new_node;
+        stack->count++;
+        return SUCCESS;
+    }
 
-		if(!tmp)
-		{
-			debug("Allocation failed");
-			return INTERNAL_ERROR;
-		}
-
-		tmp->type = type;
-		if(!value) {debug("pushing NULL pointer as the value");}
-		tmp->value = value;
-		tmp->next = found->next;
-		found->next = tmp;
-
-		stack->count++;
-
-		return SUCCESS;
-	}
-
-	debug("Type not found");
-	return INTERNAL_ERROR;
+    return INTERNAL_ERROR;
 }
 
 
