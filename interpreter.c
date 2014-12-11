269a47cd7c8b0416fc0448d0bb472c23d83323d4
shootervm <shooter.vm@gmail.com>
2014-12-11 23:24:31 +0100

    [interpreter] COPY repaired

--- interpreter.c
+++ interpreter.c
@@ -45,7 +45,7 @@ int interpret_loop(Instruction *item, Stack *calcs, Stack *locals, Stack *instru
 {
 
 	Value values[2];
-	Type  types[2];
+	Type  types[3];
 	Value result = {.inited = true};
 
 
@@ -538,12 +538,32 @@ int interpret_loop(Instruction *item, Stack *calcs, Stack *locals, Stack *instru
 
 		case I_COPY:
 			debug("I_COPY\n");
-		//TODO: dorobit stack operacie
 
-			if (types[0] == TYPE_STRING)
+			if(stack_index(calcs, 0, (int*)&types[0], &values[0])==INTERNAL_ERROR)
+			{
+				debug("Invalid read from calcs.\n");
+				return INTERNAL_ERROR;
+			}
+			if(stack_index(calcs, 1, (int*)&types[1], &values[1])==INTERNAL_ERROR)
+			{
+				debug("Invalid read from calcs.\n");
+				return INTERNAL_ERROR;
+			}
+			if(stack_index(calcs, 2, (int *)&types[2], &result) == INTERNAL_ERROR)
+			{
+				debug("Invalid read from calcs.\n");
+				return INTERNAL_ERROR;
+			}
+			if(stack_popping_spree(calcs, 3)==INTERNAL_ERROR)
+			{
+				debug("Popping spree calcs error.\n");
+				return INTERNAL_ERROR;
+			}
+
+			if (types[2] == TYPE_STRING && types[1] == TYPE_INT && types[0] == TYPE_INT )
 			{
 				debug("I_COPY - string\n");
-				result.data.string = copy(result.data.string, values[0].data.integer, values[1].data.integer);
+				result.data.string = copy(result.data.string, values[1].data.integer, values[0].data.integer);
 				stack_push(calcs, TYPE_STRING, &result);
 			}
 			else
